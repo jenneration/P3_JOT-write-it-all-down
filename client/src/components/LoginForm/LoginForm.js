60bdac38c774eecaaa4c6e738f1b050c66bf80a5
Menuka Gusain <menukagusain@gmail.com>
2021-04-20 12:33:19 -0400

    updated files

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -18,99 +18,6 @@ function LoginForm(props) {
 		}));
 	};
 
-<<<<<<< HEAD
-	const handleSubmitClick = (e) => {
-		e.preventDefault();
-		const payload = {
-			email: state.email,
-			password: state.password
-		};
-		var header = {
-			headers: {
-				'Access-Control-Allow-Origin': 'http://localhost:3000',
-				'Access-Control-Allow-Credentials': true,
-				'Access-Control-Allow-Methods': 'POST',
-				'Access-Control-Allow-Headers': 'Content-Type'
-			}
-		};
-		axios
-			.post(API_BASE_URL + '/signin', payload, header)
-			.then(function(response) {
-				if (response.status === 200) {
-					setState((prevState) => ({
-						...prevState,
-						successMessage: 'Login successful. Redirecting to home page..'
-					}));
-					redirectToHome();
-					props.showError(null);
-				} else if (response.status === 400) {
-					props.showError('Username or password do not match');
-				} else {
-					props.showError('this email is not registered');
-				}
-			})
-			.catch(function(error) {
-				console.log(error);
-			});
-	};
-	const redirectToHome = () => {
-		props.updateTitle('Home');
-		props.history.push('/home');
-	};
-	const redirectToRegister = () => {
-		props.history.push('/register');
-		props.updateTitle('Register');
-	};
-	return (
-		<div className="card col-12 col-lg-4 login-card mt-2 hv-center">
-			<form>
-				<div className="form-group text-left">
-					<label htmlFor="exampleInputEmail1">Email address</label>
-					<input
-						type="email"
-						className="form-control"
-						id="email"
-						aria-describedby="emailHelp"
-						placeholder="Enter email"
-						value={state.email}
-						onChange={handleChange}
-					/>
-					<small id="emailHelp" className="form-text text-muted">
-						We'll never share your email with anyone else.
-					</small>
-				</div>
-				<div className="form-group text-left">
-					<label htmlFor="exampleInputPassword1">Password</label>
-					<input
-						type="password"
-						className="form-control"
-						id="password"
-						placeholder="Password"
-						value={state.password}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="form-check" />
-				<button type="submit" className="btn btn-primary" onClick={handleSubmitClick}>
-					Submit
-				</button>
-			</form>
-			<div
-				className="alert alert-success mt-2"
-				style={{ display: state.successMessage ? 'block' : 'none' }}
-				role="alert"
-			>
-				{state.successMessage}
-			</div>
-			<div className="registerMessage">
-				<span>Dont have an account? </span>
-				<span className="loginText" onClick={() => redirectToRegister()}>
-					Register
-				</span>
-			</div>
-		</div>
-	);
-=======
     const handleSubmitClick = (e) => {
         e.preventDefault();
         const payload={
@@ -194,7 +101,6 @@ function LoginForm(props) {
             </div>
         </div>
     )
->>>>>>> arjunbranch
 }
 
 export default withRouter(LoginForm);
