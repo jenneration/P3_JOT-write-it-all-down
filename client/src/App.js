1c0c968818858fe9d068e816e8654d60f47f82d6
Jenner Garcia <jenner.garcia@gmail.com>
2021-04-20 22:04:15 -0400

    fixed navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import LoginForm from './components/LoginForm/LoginForm';
@@ -9,33 +9,66 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import AlertComponent from './components/AlertComponent/AlertComponent';  
+import AlertComponent from './components/AlertComponent/AlertComponent';
+
+// Site Routes
+// import Navbar from "./components/Navbar/navbar";
+import AllJournals from "./pages/AllJournals";
+import Quotes from "./pages/Quotes";
+import About from "./pages/About";
+import AllEntries from "./pages/AllEntries";
+import EntryPage from "./pages/EntryPage";
+import CreateEntry from "./pages/CreateEntry";
+
+
+
 function App() {
   const [title, updateTitle] = useState(null);
   const [errorMessage, updateErrorMessage] = useState(null);
   return (
     <Router>
-    <div className="App">
-      <Header title={title}/>
+      <div className="App">
+        <Header title={title} />
         <div className="container d-flex align-items-center flex-column">
           <Switch>
             <Route path="/" exact={true}>
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
             <Route path="/register">
-              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
             <Route path="/login">
-              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle} />
             </Route>
-            <Route path="/home">
-              <Home/>
+            <Route path="/alljournals">
+              <Home />
             </Route>
           </Switch>
-          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage} />
+          {/* </div> */}
+
+
+          {/* TO FIX START */}
+
+          {/* <div> */}
+          {/* <Navbar /> */}
+          <Route exact path="/alljournals" component={AllJournals} />
+          <Route exact path="/quotes" component={Quotes} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/entrypage" component={EntryPage} />
+          <Route exact path="/createentry" component={CreateEntry} />
+          <Route exact path="/allentries" component={AllEntries} />
         </div>
-    </div>
+      </div>
+
+
+      {/* TO FIX END */}
+
+
+
     </Router>
+
+
   );
 }
 
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,37 +1,130 @@
 // import { STATES } from 'mongoose';
- import React,{ useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 // import {useDispatch} from "react-redux";
 // import {Redirect, useHistory} from "react-router-dom";
-const Header = ()=> {
-    const[user, setUser] = useState([]);
-    useEffect(()=>{
+import { Link } from "react-router-dom";
+import "bootstrap/js/src/collapse.js";
+
+
+
+const Header = () => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
         getUser()
-    },[])
-     function getUser(){
+    }, [])
+    function getUser() {
         const user = JSON.parse(localStorage.getItem("user"))
         setUser(user)
-     }
-    const logout = ()=>{
-        localStorage.clear();
-        window.location.href="/login";
     }
-    return(
-        <nav class="navbar navbar-dark bg-primary">
-            <div className="row col-12 d-flex justify-content-center text-white align-self-center">
-            <span className="h3 align-self-center">The-JAMM</span>
+    const logout = () => {
+        localStorage.clear();
+        window.location.href = "/login";
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             {user ? (
-                <div className="profile-head align-right">
-                    
-                    <button className="btn btn-primary" style={{float: "right"}} onClick={logout}> logout</button>
-                    <h6 className="display-flex-end">{user.name}</h6>
-                 </div>
-            ) : (
-                <div className="ml-auto"> <button className="btn btn-light display-flex-end" >login/register</button></div>
-            )}
-            </div>
+                <div>
+                    <div className="navbar-brand"><Link to="/alljournals" className={window.location.pathname === "/alljournals" ? "nav-link active" : "nav-link"}>jot</Link></div>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to="/alljournals" className={window.location.pathname === "/alljounsals" ? "nav-link active" : "nav-link"}>Home/AllJournals</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/allentries" className={window.location.pathname === "/allentries" ? "nav-link active" : "nav-link"}>AllEntries</Link>
+                            </li>
+                            <li className="nav-item" >
+                                <Link to="/createentry" className={window.location.pathname === "/createentry" ? "nav-link active" : "nav-link"}>CreateEntry</Link>
+                            </li>
+                            <li className="nav-item" >
+                                <Link to="/quotes" className={window.location.pathname === "/quotes" ? "nav-link active" : "nav-link"}>Quotes</Link>
+                            </li>
+                            <li className="nav-item" >
+                                <Link to="/entrypage" className={window.location.pathname === "/entrypage" ? "nav-link active" : "nav-link"}>EntryPage</Link>
+                            </li>
+                            <li className="nav-item" >
+                                <Link to="/about" className={window.location.pathname === "/entries" ? "nav-link active" : "nav-link"}>About</Link>
+                            </li>
+                            <li>
+                                <button className="btn btn-primary" onClick={logout}> logout</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>) : (<div><a href="/login"><button className="btn btn-light " >login</button></a></div>)}
+
         </nav>
-       
-        
+    )
+    return (
+
+
+
+
+
+
+
+
+
+        <nav className="navbar navbar-light bg-light ">
+            {/* <div className="row col-12 d-flex "> */}
+            {/* <span className="h3">The-JAMM</span> */}
+            {user ? (
+                <div>
+
+
+
+                    {/* TO FIX START */}
+                    <span className="navbar-brand">
+                        <Link to="/alljournals" className={window.location.pathname === "/alljournals" ? "nav-link active" : "nav-link"}>jot</Link>
+
+                    </span>
+                    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button> */}
+
+                    {/* <div className="collapse navbar-collapse text-black" id="navbarNavAltMarkup"> */}
+                    <ul className="navbar-nav">
+                        <li className="nav-item" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <Link to="/alljournals" className={window.location.pathname === "/alljounsals" ? "nav-link active" : "nav-link"}>Home/AllJournals</Link>
+                        </li>
+                        <li className="nav-item" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <Link to="/allentries" className={window.location.pathname === "/allentries" ? "nav-link active" : "nav-link"}>AllEntries</Link>
+                        </li>
+                        <li className="nav-item" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <Link to="/createentry" className={window.location.pathname === "/createentry" ? "nav-link active" : "nav-link"}>CreateEntry</Link>
+                        </li>
+                        <li className="nav-item" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <Link to="/quotes" className={window.location.pathname === "/quotes" ? "nav-link active" : "nav-link"}>Quotes</Link>
+                        </li>
+                        <li className="nav-item" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <Link to="/entrypage" className={window.location.pathname === "/entrypage" ? "nav-link active" : "nav-link"}>EntryPage</Link>
+                        </li>
+                        <li className="nav-item" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <Link to="/about" className={window.location.pathname === "/entries" ? "nav-link active" : "nav-link"}>About</Link>
+                        </li>
+                    </ul>
+                    {/* </div> */}
+
+                    {/* TO FIX END */}
+
+
+
+
+
+                    <button className="btn btn-primary" onClick={logout}> logout</button>
+                    <h6 >{user.name}</h6>
+                </div>
+            ) : (
+
+                <div className="ml-auto"> <button className="btn btn-light " >login/register</button></div>
+            )}
+            {/* </div> */}
+        </nav >
+
+
     )
 }
 export default Header;
