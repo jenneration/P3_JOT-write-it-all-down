3e2cb020c502e2587daca432dcdf5e7ddcc32fa5
Mary C <mcn4c@virginia.edu>
2021-04-21 20:38:33 -0400

    added theme selectors and quotes.json file

--- client/src/pages/CreateEntry.js
+++ client/src/pages/CreateEntry.js
@@ -1,15 +1,40 @@
-import React from "react";
-import { Container, Row, Col } from "../components/Grid/grid";
+import React, {useState} from 'react';
+import {  Row, Col } from "../components/Grid/grid";
 import { Input, TextArea, FormBtn } from "../components/Form/form";
 // import SideNav from "../components/SideNav";
 import Side from "../components/Side/side";
+import ThemeDropdown from "../components/ThemeHeader"
+import themes from "../themes"
 
 
 function CreateEntry() {
-    return (
-        <div>
 
-            {/* <Container> */}
+
+    const [theme, setTheme] = useState('');
+
+	const handleChange = (selectedTheme) => {
+		setTheme(themes[selectedTheme.value]);
+	};
+	const refCallback = (node) => {
+		if (node) {
+		  theme &&
+			Object.keys(theme).forEach((element) => {
+			  node.style.setProperty(element, theme[element], 'important');
+			  if (element === 'background-color' || element === 'background' ) {
+				// apply the same background mentioned for theme to the body of the website
+				document.body.style.background = theme[element];
+			  }
+			});
+		}
+	  };
+    return (
+
+
+        
+            <div ref={refCallback}>
+            <ThemeDropdown handleChange={handleChange} />
+            <br></br><br></br>
+
             <Row>
                 <Col size="md-3">
                     <Side />
@@ -31,7 +56,6 @@ function CreateEntry() {
                 </Col>
             </Row>
 
-            {/* </Container> */}
 
         </div>
     )
