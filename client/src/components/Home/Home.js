2adabe222c04d23218aad619b106b1d2d4e6e585
Jenner Garcia <jenner.garcia@gmail.com>
2021-04-24 11:36:20 -0400

    ok

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,96 +1,100 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 //import { API_BASE_URL } from '../../constants/apiContants';
 // import {API_BASE_URL} from '../../constants/apiContants';
 
 function Home() {
-    const [state , setState] = useState({
+    const [state, setState] = useState({
         journal: "",
         successMessage: null,
-        userId : "",
-        token :"",
-        result:[]
+        userId: "",
+        token: "",
+        result: []
     })
     var user = JSON.parse(localStorage.getItem('user'));
     const handleChange = (e) => {
-        const {name , value} = e.target 
-        if(user){
+        const { name, value } = e.target
+        if (user) {
             setState(prevState => ({
                 ...prevState,
-                [name] : value,
+                [name]: value,
                 userId: user.id,
                 token: user.token,
-                name:user.name
+                name: user.name
             }))
-        }  
-      
+        }
+
     }
-    const addJournal = (e)=>{
+    const addJournal = (e) => {
         e.preventDefault();
-        if(state.journal==="")return;
+        if (state.journal === "") return;
         axios.post('http://localhost:3001/user/journal', state)
-        .then(res =>
-            console.log(res)
-        )
-        .catch(err =>{
-            console.log(err)
-        })
+            .then(res =>
+                console.log(res)
+            )
+            .catch(err => {
+                console.log(err)
+            })
     }
-    const getJournal = async (e)=>{
+    const getJournal = async (e) => {
         e.preventDefault();
         const user = JSON.parse(localStorage.getItem('user'));
-        if(!user)return window.location.href ="/login";
+        if (!user) return window.location.href = "/login";
         const apiUrl = "http://localhost:3001/user/"
         const authAxios = axios.create({
-        baseURL: apiUrl,
-        headers:{
-            Authorization :`Bearer ${user.token} `,
-            userId: user.id
-         }
+            baseURL: apiUrl,
+            headers: {
+                Authorization: `Bearer ${user.token} `,
+                userId: user.id
+            }
         })
-        if(state.journal==="")return;
+        if (state.journal === "") return;
         // Capturing the Token
         try {
-        const journal = await  (authAxios.get(`journal/${state.userId}`));
-        console.log(journal)
-            if(journal){  setState(prevState => ({
-                ...prevState,
-                result:journal.data
-            }))}
-          
-        
+            const journal = await (authAxios.get(`journal/${state.userId}`));
+            console.log(journal)
+            if (journal) {
+                setState(prevState => ({
+                    ...prevState,
+                    result: journal.data
+                }))
+            }
+
+
         } catch (error) {
-        console.log(error)
-        }  
+            console.log(error)
+        }
     }
 
-    return(
+    return (
+
         <div>
-        <div className="mt-2">
-           <form>
-               <input name="journal" type="text" placeholder="name of the journal" onChange={handleChange} />
-               <button type="submit" onClick={addJournal}>add journal</button>
-           </form>
-           <form>
-              
-               <button type="submit" onClick={getJournal}>get journal</button>
-           </form>
+            <div className="mt-2">
+                <form>
+                    <input name="journal" type="text" placeholder="name of the journal" onChange={handleChange} />
+                    <button type="submit" onClick={addJournal}>add journal</button>
+                </form>
+                <form>
+
+                    <button type="submit" onClick={getJournal}>get journal</button>
+                </form>
+            </div>
+            {state.result ? (
+                <ul>
+                    {state.result.map(item => (
+                        <li key={item._id}>
+                            <li to={"/books/" + item._id}>
+                                <strong>
+                                    {item.name}
+                                </strong>
+                            </li>
+                            <button>btn</button>
+                        </li>
+                    ))}
+                </ul>
+            ) : (<div>button1 </div>)}
+
         </div>
-        {state.result? (
-            <ul>
-                {state.result.map(item => (
-                  <li key={item._id}>
-                    <li to={"/books/" + item._id}>
-                      <strong>
-                        {item.name}
-                      </strong>
-                    </li>
-                    <button>btn</button>
-                  </li>
-                ))}
-            </ul>
-        ):(<div>button1 </div>)}
-    </div>
     )
 
 }
--- client/src/pages/CreateEntry.js
+++ client/src/pages/CreateEntry.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
-import {  Row, Col } from "../components/Grid/grid";
+import React, { useState } from 'react';
+import { Row } from "../components/Grid/grid";
 import { Input, TextArea, FormBtn } from "../components/Form/form";
-// import SideNav from "../components/SideNav";
-import Side from "../components/Side/side";
+import Wrapper from "../components/Wrapper";
 import ThemeDropdown from "../components/ThemeHeader"
 import themes from "../themes"
 
@@ -12,46 +11,47 @@ function CreateEntry() {
 
     const [theme, setTheme] = useState('');
 
-	const handleChange = (selectedTheme) => {
-		setTheme(themes[selectedTheme.value]);
-	};
-	const refCallback = (node) => {
-		if (node) {
-		  theme &&
-			Object.keys(theme).forEach((element) => {
-			  node.style.setProperty(element, theme[element], 'important');
-			  if (element === 'background-color' || element === 'background' ) {
-				// apply the same background mentioned for theme to the body of the website
-				document.body.style.background = theme[element];
-			  }
-			});
-		}
-	  };
+    const handleChange = (selectedTheme) => {
+        setTheme(themes[selectedTheme.value]);
+    };
+    const refCallback = (node) => {
+        if (node) {
+            theme &&
+                Object.keys(theme).forEach((element) => {
+                    node.style.setProperty(element, theme[element], 'important');
+                    if (element === 'background-color' || element === 'background') {
+                        // apply the same background mentioned for theme to the body of the website
+                        document.body.style.background = theme[element];
+                    }
+                });
+        }
+    };
     return (
+        <Wrapper>
             <div ref={refCallback}>
-            <ThemeDropdown handleChange={handleChange} />
-            <br></br><br></br>
-            <Row>
-                <Col size="md-3">
-                    <Side />
-                </Col>
-                <Col size="md-6">
-                    <form>
-                        <FormBtn>Save</FormBtn>
-                        <Input
-                            // onChange={handleInputChange}
-                            name="title"
-                            placeholder="Title" />
-                        <Input
-                            name="dateCreated/Datenow()"
-                            placeholder="Today's date/DateCreated" />
-                        <TextArea
-                            name="entrycontent"
-                            placeholder="Jot it here" />
-                    </form>
-                </Col>
-            </Row>
-        </div>
+                <ThemeDropdown handleChange={handleChange} />
+                <br></br><br></br>
+                <div className="container">
+                    <Row className="d-flex justify-content-center">
+                        <div className="col-md-7 offset-md-2">
+                            <form className="form-create">
+                                <FormBtn>Save</FormBtn>
+                                <Input
+                                    // onChange={handleInputChange}
+                                    name="title"
+                                    placeholder="Title" />
+                                <Input
+                                    name="today's date goes here"
+                                    placeholder="Today's Date" />
+                                <TextArea
+                                    name="entrycontent"
+                                    placeholder="Jot it here" />
+                            </form>
+                        </div>
+                    </Row>
+                </div>
+            </div>
+        </Wrapper >
     )
 }
 
